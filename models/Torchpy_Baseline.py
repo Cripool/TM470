6348c8db0ce60f9fe450aab4377a34883c8dada1
Brendan <nova-vita@outlook.com>
2026-04-04 00:45:26 +0100

    Started and updated new CNN Using Pytorch

--- models/Torchpy_Baseline.py
+++ models/Torchpy_Baseline.py
@@ -1,4 +1,6 @@
 import torch
+import pandas as pd
+import numpy as np
 import torchvision
 import torch.nn as nn
 import torch.optim as optim
@@ -7,11 +9,6 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, classification_report
-
-
-import os
-
-print(os.listdir("data/processed/test"))
 
 # ---Data Transform --- 
 
